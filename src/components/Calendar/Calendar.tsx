41db91a727ba3e6c3eb72f07424f0fb16369839b
Vlad Sovetov <vlad.sovetov@gmail.com>
2023-09-05 10:56:27 +0300

    feat: Month navigation

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -2,6 +2,22 @@ import { CalendarEvent } from "@/types";
 import styled, { css } from "styled-components";
 import { getCalendarCells } from "./utils";
 import { CalendarCell } from "./CalendarCell";
+import { useState } from "react";
+
+const CalendarNavigation = styled.div`
+  display: flex;
+  margin-bottom: 16px;
+`;
+const CalendarNavigationButton = styled.button`
+  height: 30px;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #6565f1;
+  color: white;
+  cursor: pointer;
+`;
 
 const CalendarGridColumnsCss = css`
   grid-template-columns: repeat(7, 1fr);
@@ -31,12 +47,26 @@ interface CalendarProps {
 }
 
 export const Calendar = ({ events }: CalendarProps) => {
-  const date = new Date();
+  const [date, setDate] = useState(new Date());
   const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
   const cells = getCalendarCells(date.getFullYear(), date.getMonth());
+  const navigateMonthDate = (direction: number) => {
+    const nextMonthDate = new Date(date);
+    nextMonthDate.setMonth(nextMonthDate.getMonth() + direction);
+    setDate(nextMonthDate);
+  };
   return (
     <div>
+      <CalendarNavigation>
+        <CalendarNavigationButton onClick={() => navigateMonthDate(-1)}>
+          {" "}
+          Prev Month
+        </CalendarNavigationButton>
+        <CalendarNavigationButton onClick={() => navigateMonthDate(1)}>
+          Next Month
+        </CalendarNavigationButton>
+      </CalendarNavigation>
       <CalendarHeaderGrid>
         {weekDays.map((day) => (
           <WeekdayLabel key={day}>{day}</WeekdayLabel>
